f3248ef3e88fa31f06d47df7255ee050a57705bd
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-11 22:16:27 +0300

    Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
-  { path: '', component: ProductsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
   { path: 'cart', component: CartComponent },
+  { path:'*', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/github", pathMatch:"full"},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
